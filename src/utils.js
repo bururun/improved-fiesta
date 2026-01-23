2fcd80445362cfd3c6006d618de3bb1e90bbc429
bururun <bururun@users.noreply.github.com>
2026-01-23 15:07:41 +0200

    ci: code cleanup

--- src/utils.js
+++ src/utils.js
@@ -20,3 +20,27 @@ function newFunction7() {
 }
 
 module.exports = { formatData, validateInput };
+
+
+// Utility functions for EventManager
+
+function formatData(data) {
+    if (typeof data === 'string') {
+        return data.trim().toUpperCase();
+    }
+    return data;
+}
+
+function validateInput(value) {
+    if (value === null || value === undefined) {
+        throw new Error('Value cannot be null');
+    }
+    return true;
+}
+
+// Update 24
+function newFunction24() {
+    return 24;
+}
+
+module.exports = { formatData, validateInput };
