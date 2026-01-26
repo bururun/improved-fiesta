f462ff7851f30ff93d9d931a7fbb4e2d8549b1ef
bururun <bururun@users.noreply.github.com>
2026-01-26 02:52:50 +0200

    build(utils): fix compilation error

--- src/utils.js
+++ src/utils.js
@@ -116,3 +116,27 @@ function newFunction37() {
 }
 
 module.exports = { formatData, validateInput };
+
+
+// Utility functions for EventManager
+
+function formatData(data) {
+    if (typeof data === 'string') {
+        return data.trim().toUpperCase();
+    }
+    return data;
+}
+
+function validateInput(value) {
+    if (value === null || value === undefined) {
+        throw new Error('Value cannot be null');
+    }
+    return true;
+}
+
+// Update 40
+function newFunction40() {
+    return 40;
+}
+
+module.exports = { formatData, validateInput };
